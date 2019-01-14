4c3ddaa62e3748c52247c85002e37ca5860d27ac
InahoAZ <santyyvii@gmail.com>
2019-01-14 19:49:53 -0300

    pruebita xd

--- polls/views.py
+++ polls/views.py
@@ -7,4 +7,4 @@ def index(request):
 	return HttpResponse("Hello, world. You're at the polls index.")
 
 def coso(request):
-	return HttpResponse("Hola Arnoldo")
+	return render(request, 'blog/coso.html')
