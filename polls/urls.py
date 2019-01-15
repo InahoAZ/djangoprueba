509534cdfd86c9d5bcb3804b590c6295a7030bb3
InahoAZ <santyyvii@gmail.com>
2019-01-15 11:56:07 -0300

    cambios

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('coso/', views.coso, name='coso'),
+    path('coso/<int:pk>', views.post_detail, name='post_detail'),
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 # Create your views here.
 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def coso(request):
-	query = Question.objects.all()
-	return render(request, 'blog/coso.html',{'query': query})
+    query = Question.objects.all()
+    return render(request, 'blog/coso.html', {'query': query})
+
+
+def post_detail(request, pk):
+    query = get_object_or_404(Question, pk=pk)
+    return render(request, 'blog/post_detail.html', {'query': query})
